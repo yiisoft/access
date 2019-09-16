a00230063479745cabef9009221a3928a0f99c84
Alexander Makarov <sam@rmcreative.ru>
2019-09-16 22:03:48 +0300

    Fix #1: adjust naming

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
     <br>
 </p>
 
-This package provides an interface for checking access.
+This package provides an interface for checking if user has certain permission.
 
 [![Latest Stable Version](https://poser.pugx.org/yiisoft/access/v/stable.png)](https://packagist.org/packages/yiisoft/access)
 [![Total Downloads](https://poser.pugx.org/yiisoft/access/downloads.png)](https://packagist.org/packages/yiisoft/access)
--- src/AccessCheckerInterface.php
+++ src/AccessCheckerInterface.php
@@ -5,19 +5,18 @@ namespace Yiisoft\Access;
  * For more details and usage information on CheckAccessInterface, see
  * the [guide article on security authorization](guide:security-authorization).
  */
-interface CheckAccessInterface
+interface AccessCheckerInterface
 {
     /**
      * Checks if the user has the specified permission.
      *
      * @param mixed $userId the user ID representing the unique identifier of a user.
-     * @param string $permissionName the name of the permission to be checked against
-     * @param array $parameters name-value pairs that will be passed to the rules associated
-     * with the roles and permissions assigned to the user.
+     * @param string $permissionName the name of the permission to be checked against.
+     * @param array $parameters name-value pairs that will used to determine if access is granted.
      *
      * @return bool whether the user has the specified permission.
      *@throws \InvalidArgumentException if $permissionName does not refer to an existing permission
      *
      */
-    public function checkAccess($userId, string $permissionName, array $parameters = []): bool;
+    public function hasPermission($userId, string $permissionName, array $parameters = []): bool;
 }
