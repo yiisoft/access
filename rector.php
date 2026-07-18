6cd5d8373db4465652ce9efb30f0210a046fff79
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-07-18 21:55:10 +0300

    cs

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,4 +14,7 @@ return RectorConfig::configure()
     ->withPhpSets(php80: true)
     ->withRules([
         InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
+        ClosureToArrowFunctionRector::class,
     ]);
