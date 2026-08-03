6dd71f42af566c731907362b3582c31d5adea8f9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-03 23:51:59 +0300

    revert

--- src/AccessCheckerInterface.php
+++ src/AccessCheckerInterface.php
@@ -8,7 +8,7 @@ use InvalidArgumentException;
 use Stringable;
 
 /**
- * The inteqrface defines checking if certain user has certain permission. Optional parameters could be passed
+ * The interface defines checking if certain user has certain permission. Optional parameters could be passed
  * for fine-grained access checks.
  */
 interface AccessCheckerInterface
